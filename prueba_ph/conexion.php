6fa278b984a41755eb031a2a75a82b3a631c5368
Stiven-38 <129075107+Stiven-38@users.noreply.github.com>
2023-09-18 07:33:49 -0500

    subir

---
+++ prueba_ph/conexion.php
@@ -0,0 +1,13 @@
+<?php
+
+$conexion = new mysqli('localhost', 'root', '', 'prueba_datos');
+if ($conexion ->connect_errno){
+    die ('lo sineto hubo un problema en la conexion');
+    }else{
+        $sql = "INSERT INTO usuarios(ID,nombre,edad) VALUES(null,'sonia',30)";
+        $resultado =$conexion->query($sql);
+        if($conexion->affected_row  >=1){
+            echo 'filas agredas:' . $conexion->affected_row;
+        }
+    }
+?>
